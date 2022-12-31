80e2a223140091da8721a177130b74a620dab85e
Zahra <zahra.yalameh1367@gmail.com>
2022-12-31 16:54:21 +0330

    hello

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('hello')
 })
 app.listen(process.env.PORT || 3000)
