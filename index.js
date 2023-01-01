fe21ee9fd8b742105c231893f38a2d269e72972d
Zahra <zahra.yalameh1367@gmail.com>
2023-01-01 09:20:06 +0330

    instagram

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const app = express()
 const Insta = require('scraper-instagram');
 const InstaClient = new Insta();
 
-const result = 'fffff';
+const result = 'fffff2';
 
 const yourSessionId = '6982271705%3ACqFOjDIwgfTBQH%3A13%3AAYf87RFOSbrI9-emBqZL450hctPFEeeV3aZbDL0nAQ';
 
 InstaClient.authBySessionId(yourSessionId)
 	.then(account => {result = account;} )
-	.catch(err => {result = err;} )
-	.finaly(err => {result = err;} );
+	.catch(err => {result = err;} );
 
     app.all('/', (req, res) => {
         console.log("Just got a request!")
-        res.send(result)
+        res.send(InstaClient)
     })
 
  
