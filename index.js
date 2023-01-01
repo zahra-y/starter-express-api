b9fbb226aa5c26f7b8a22ab4702d832b4a5a6aee
Zahra <zahra.yalameh1367@gmail.com>
2023-01-01 09:38:33 +0330

    instagram

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-
+const {format} = require('date-fns')
 
 const result = 'fffff2';
+const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
 
 var ig = require('instagram-scraping');
 
@@ -13,7 +14,7 @@ ig.scrapeTag('veranda').then((result2) => {
 
     app.all('/', (req, res) => {
         console.log("Just got a request!")
-        res.send(result)
+        res.send(dateTime)
     })
 
  
