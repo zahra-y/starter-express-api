7e6fd0717394f0c5042b2e1a247a5e25b5bbf072
Zahra <zahra.yalameh1367@gmail.com>
2023-01-02 16:30:34 +0330

    pup

--- index.js
+++ index.js
@@ -6,34 +6,51 @@ fs = require('fs');
 const puppeteer = require('puppeteer');
 const url = 'https://www.instagram.com/p/CmwOewqvd2W/?utm_source=ig_web_copy_link';
 
-puppeteer
-  .launch()
-  .then(function(browser) {
-    return browser.newPage();
-  })
-  .then(function(page) {
-    return page.goto(url).then(function() {
-      return page.content();
-    });
-  })
-  .then(function(html) {
-    //console.log(html);
 
-    const $ = cheerio.load(html);
+let bot = require('./puppeteer');
+let cnf = require('./config/config.json');
+bot();
 
-    $('h2', html).each(function() {
-        console.log('h2',$(this).text());
-      });
+// fs.readFile('./views/tmp.html', 'utf8', (err, html) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+
+//   });
+
+
+//setInterval(bot, cnf.settings.run_every_x_hours * 3600000);
+
+
+// puppeteer
+//   .launch()
+//   .then(function(browser) {
+//     return browser.newPage();
+//   })
+//   .then(function(page) {
+//     return page.goto(url, {timeout: 60000}).then(function() {
+//       return page.content();
+//     });
+//   })
+//   .then(function(html) {
+//     //console.log(html);
+
+//     const $ = cheerio.load(html);
+
+//     $('h2', html).each(function() {
+//         console.log('h2',$(this).text());
+//       });
       
-    fs.writeFile('./views/tmp.html', html, function (err) {
-                if (err) return console.log(err);
-                console.log('Hello World > helloworld.txt');
-             });
+//     fs.writeFile('./views/tmp.html', html, function (err) {
+//                 if (err) return console.log(err);
+//                 console.log('Hello World > helloworld.txt');
+//              });
 
-  })
-  .catch(function(err) {
-    //handle error
-  });
+//   })
+//   .catch(function(err) {
+//     //handle error
+//   });
 
 
 
